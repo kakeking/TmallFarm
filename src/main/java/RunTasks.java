d3a710f3ce57dedb604609de0c8e24a425cff4c2
Hannah Jia <qjia8383@uni.sydney.edu.au>
2020-08-22 18:24:07 +1000

    Locate coordinate on game canvas, doing task, press fertilise

--- src/main/java/RunTasks.java
+++ src/main/java/RunTasks.java
@@ -4,12 +4,16 @@ import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
+import io.appium.java_client.touch.WaitOptions;
 
 import java.net.MalformedURLException;
+import java.time.Duration;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import static io.appium.java_client.touch.WaitOptions.waitOptions;
 import static io.appium.java_client.touch.offset.PointOption.point;
+import static java.time.Duration.ofSeconds;
 
 public class RunTasks {
 
@@ -43,7 +47,7 @@ public class RunTasks {
         pwd.sendKeys(props.getProperty("password"));
         driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 
-        //         get source to check element, this page cannot be captured by Appium inspector or UIautomatorViewer
+//        get page source to check element, this page cannot be captured by Appium inspector or UIautomatorViewer
 //        try {
 //            Thread.sleep(6000);
 //        } catch (InterruptedException e) {
@@ -51,12 +55,76 @@ public class RunTasks {
 //        }
 //        String source = driver.getPageSource();
 //        System.out.println(source);
+
         MobileElement login = Utils.waitForClickable(driver, (MobileElement) driver.findElement(MobileBy.id("com.taobao.taobao:id/aliuser_recommend_login_next_btn")));
         login.click();
+//        wait for page to load
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         MobileElement farm = Utils.waitForClickable(driver, (MobileElement) driver.findElement(MobileBy.xpath("//android.widget.FrameLayout[@content-desc=\"芭芭农场\"]")));
         farm.click();
-        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
-        TouchAction touchAction=new TouchAction(driver);
-        touchAction.press(point(100, 800)).perform();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//      This block is used to locate coordinate of entry, this element cannot be located in Appium inspector or
+//      Uiautomatorviewer as the whole screen is a game canvas.
+//        int x = 160;
+//        int y = 710;
+//        while (x< 175) {
+//            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+//            System.out.println("touch point: "+x +","+y);
+//            TouchAction touchAction = new TouchAction(driver);
+//            touchAction.press(point(x, y)).moveTo(point(x+1, y+1)).release().perform();
+//            x += 1;
+//            y += 2;
+//        }
+
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        TouchAction touchAction = new TouchAction(driver);
+        touchAction.press(point(160, 710)).moveTo(point(161, 711)).release().perform();
+//      Press collect fertiliser wait for page to load
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        touchAction.press(point(950, 1570)).release().perform();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        if(!driver.findElements(MobileBy.xpath("//android.widget.Button[@text='去签到']")).isEmpty()){
+            driver.findElement(MobileBy.xpath("//android.widget.Button[@text='去签到']")).click();
+        }
+        if(!driver.findElements(MobileBy.xpath("//android.widget.Button[@text='去领取']")).isEmpty()){
+            driver.findElement(MobileBy.xpath("//android.widget.Button[@text='去领取']")).click();
+        }
+        while(!driver.findElements(MobileBy.xpath("//android.widget.Button[@text='去浏览']")).isEmpty()){
+            driver.findElement(MobileBy.xpath("//android.widget.Button[@text='去浏览']")).click();
+            try {
+                Thread.sleep(22000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+//            press back on Image button with no name, id
+//            int x = 45;
+//            int y = 100;
+//            while (x< 54) {
+//            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+//            System.out.println("touch point: "+x +","+y);
+//            touchAction.press(point(x, y)).release().perform();
+//            x += 2;
+//            y += 2;
+//        }
+            touchAction.press(point(45, 100)).release().perform();
+            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        }
+        driver.findElement(MobileBy.xpath("//android.widget.Button[@text='关闭']")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        int i =15;
+        while (i>0){
+            touchAction.press(point(450, 1570)).waitAction(waitOptions(ofSeconds(2))).release().perform();
+            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+            i--;
+        }
     }
 }
--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,5 +1,7 @@
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,6 +14,10 @@ import java.net.URL;
 import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.logging.Logger;
+
+import static io.appium.java_client.touch.WaitOptions.waitOptions;
+import static io.appium.java_client.touch.offset.PointOption.point;
+import static java.time.Duration.ofSeconds;
 
 public class Utils {
     private static final Properties PROPERTY = new Properties();
@@ -46,18 +52,36 @@ public class Utils {
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability("deviceName", props.getProperty("deviceName"));
         cap.setCapability("udid", props.getProperty("udid"));
-        cap.setCapability("platformName", "Android");
+        cap.setCapability("platformName", props.getProperty("platform"));
         cap.setCapability("platformVersion", props.getProperty("androidVersion"));
         cap.setCapability("appPackage", "com.taobao.taobao");
         cap.setCapability("appActivity", "com.taobao.tao.TBMainActivity");
         cap.setCapability("autoGrantPermissions", "true");
         cap.setCapability("newCommandTimeout", 50);
-//        cap.setCapability("unicodeKeyboard", true);
-//        cap.setCapability("resetKeyboard", true);
+        cap.setCapability("unicodeKeyboard", true);
+        cap.setCapability("resetKeyboard", true);
 
         URL url = new URL("http://127.0.0.1:4723/wd/hub");
         AndroidDriver driver = new AndroidDriver(url, cap);
         System.out.println("Application started...");
         return driver;
+    }
+
+    public static void swipeDown(AndroidDriver driver, int howManySwipes) {
+        Dimension size = driver.manage().window().getSize();
+        // calculate coordinates for vertical swipe
+        int startVerticalY = (int) (size.height * 0.3);
+        int endVerticalY = (int) (size.height * 0.7);
+        int startVerticalX = (int) (size.width / 2.1);
+        try {
+            for (int i = 1; i <= howManySwipes; i++) {
+                new TouchAction<>(driver).longPress(point(startVerticalX, startVerticalY))
+                        .waitAction(waitOptions(ofSeconds(2))).moveTo(point(startVerticalX, endVerticalY))
+                        .release().perform();
+                System.out.println(startVerticalX+", "+startVerticalY);
+            }
+        } catch (Exception e) {
+            LOGGER.info("Exception in the swipedown method of the util class" + e);
+        }
     }
 }
