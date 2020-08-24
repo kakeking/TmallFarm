5c5c91c47fa5a50e40c264a5c32574f917c502d2
Hannah Jia <qjia8383@uni.sydney.edu.au>
2020-08-24 10:49:42 +1000

    update wait untill function, add steps

--- src/main/java/RunTasks.java
+++ src/main/java/RunTasks.java
@@ -5,6 +5,9 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.touch.WaitOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.time.Duration;
@@ -59,14 +62,16 @@ public class RunTasks {
         MobileElement login = Utils.waitForClickable(driver, (MobileElement) driver.findElement(MobileBy.id("com.taobao.taobao:id/aliuser_recommend_login_next_btn")));
         login.click();
 //        wait for page to load
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(driver, 120);
+        Utils.waitForElement(driver, By.xpath("//android.widget.FrameLayout[@content-desc=\"芭芭农场\"]"));
         MobileElement farm = Utils.waitForClickable(driver, (MobileElement) driver.findElement(MobileBy.xpath("//android.widget.FrameLayout[@content-desc=\"芭芭农场\"]")));
         farm.click();
         try {
-            Thread.sleep(10000);
+            Thread.sleep(15000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+//        Utils.waitForElement(driver, By.xpath("//android.widget.Image[@text='TB18vyNEFP7gK0jSZFjXXc5aXXa-56-56'])"));
 //      This block is used to locate coordinate of entry, this element cannot be located in Appium inspector or
 //      Uiautomatorviewer as the whole screen is a game canvas.
 //        int x = 160;
@@ -79,21 +84,34 @@ public class RunTasks {
 //            x += 1;
 //            y += 2;
 //        }
-
+//      close overlay before tap entry (this overlay appears once after 8 hours)
+        if(!driver.findElements(MobileBy.xpath("//android.view.View[@text='立即去收']")).isEmpty()){
+            driver.findElement(MobileBy.xpath("//android.view.View[@text='立即去收']")).click();
+        }
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         TouchAction touchAction = new TouchAction(driver);
         touchAction.press(point(160, 710)).moveTo(point(161, 711)).release().perform();
 //      Press collect fertiliser wait for page to load
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(10000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        Utils.waitForElement(driver, By.xpath("//android.view.View[@text='合种']"));
 
-        touchAction.press(point(950, 1570)).release().perform();
+//        touchAction.press(point(800, 1270)).release().perform();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        Boolean menuOpen = false;
+        while (driver.findElements(MobileBy.xpath("//android.widget.Button[@text='去签到']")).isEmpty()){
+            int x = 940;
+            int y = 1570;
+            touchAction.press(point(x, y)).release().perform();
+            x+=5;
+            y+=2;
+        }
         if(!driver.findElements(MobileBy.xpath("//android.widget.Button[@text='去签到']")).isEmpty()){
             driver.findElement(MobileBy.xpath("//android.widget.Button[@text='去签到']")).click();
+            menuOpen = true;
         }
         if(!driver.findElements(MobileBy.xpath("//android.widget.Button[@text='去领取']")).isEmpty()){
             driver.findElement(MobileBy.xpath("//android.widget.Button[@text='去领取']")).click();
@@ -101,27 +119,45 @@ public class RunTasks {
         while(!driver.findElements(MobileBy.xpath("//android.widget.Button[@text='去浏览']")).isEmpty()){
             driver.findElement(MobileBy.xpath("//android.widget.Button[@text='去浏览']")).click();
             try {
-                Thread.sleep(22000);
+                Thread.sleep(25000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-//            press back on Image button with no name, id
-//            int x = 45;
-//            int y = 100;
-//            while (x< 54) {
-//            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-//            System.out.println("touch point: "+x +","+y);
-//            touchAction.press(point(x, y)).release().perform();
-//            x += 2;
-//            y += 2;
-//        }
             touchAction.press(point(45, 100)).release().perform();
             driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         }
-        driver.findElement(MobileBy.xpath("//android.widget.Button[@text='关闭']")).click();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        int i =15;
-        while (i>0){
+        if(!driver.findElements(MobileBy.xpath("//android.widget.Button[@text='去逛逛']")).isEmpty()){
+            driver.findElement(MobileBy.xpath("//android.widget.Button[@text='去逛逛']")).click();
+            try {
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            driver.findElement(MobileBy.xpath("//android.view.View[@text='继续赚肥料']")).click();
+            touchAction.press(point(800, 1270)).release().perform();
+            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+            touchAction.press(point(945, 1570)).release().perform();
+            if(!driver.findElements(MobileBy.xpath("//android.view.View[@text='领取']")).isEmpty()){
+                driver.findElement(MobileBy.xpath("//android.view.View[@text='领取']")).click();
+            }
+            if(!driver.findElements(MobileBy.xpath("//android.view.View[@text='去签到']")).isEmpty()){
+                driver.findElement(MobileBy.xpath("//android.view.View[@text='去签到']")).click();
+            }
+            if(!driver.findElements(MobileBy.xpath("//android.view.View[@text='去逛逛']")).isEmpty()){
+                driver.findElement(MobileBy.xpath("//android.view.View[@text='去逛逛']")).click();
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if(menuOpen) {
+            driver.findElement(MobileBy.xpath("//android.widget.Button[@text='关闭']")).click();
+            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        }
+        int i =17;
+        while (i>0) {
             touchAction.press(point(450, 1570)).waitAction(waitOptions(ofSeconds(2))).release().perform();
             driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
             i--;
--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,6 +1,7 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -26,6 +27,7 @@ public class Utils {
 
     public static MobileElement waitForVisible(AndroidDriver driver, MobileElement element) {
         WebDriverWait wait = new WebDriverWait(driver, 120);
+//        wait.until(ExpectedConditions.presenceOfElementLocated(element));
         wait.until(ExpectedConditions.visibilityOf(element));
         return element;
     }
@@ -34,6 +36,10 @@ public class Utils {
         WebDriverWait wait = new WebDriverWait(driver, 120);
         wait.until(ExpectedConditions.elementToBeClickable(element));
         return element;
+    }
+    public static void waitForElement(AndroidDriver driver, By by) {
+        WebDriverWait wait = new WebDriverWait(driver, 120);
+        wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
     public static Properties loadVariables(String propertyfile){
